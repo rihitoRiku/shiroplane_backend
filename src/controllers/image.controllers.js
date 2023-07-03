13f9af6f7984f93eed920a44a16b61eb1185424a
slvally <fieshidiq@gmail.com>
2023-07-04 01:13:40 +0700

    post image

--- src/controllers/image.controllers.js
+++ src/controllers/image.controllers.js
@@ -1,88 +1,117 @@
-import Image from '../models/image.models.js';
+import Image from "../models/image.models.js";
 
 export const getImages = async (
-    /** @type import('express').Request */ req,
-    /** @type import('express').Response */ res,
-  ) => {
-    const query = req.query.new;
-    try {
-      const image = query ? await Image.find().sort({ _id: -1 }) : await Image.find();
-      if (!image) {
-        const response = res.status(403).json({
-          status: 'fail',
-          message: 'Image not found',
-        });
-        return response;
-      }
-      const response = res.status(200).json({
-        status: 'success',
-        data: image,
-      });
-      return response;
-    } catch (err) {
-      const response = res.status(500).json({
-        status: 'fail',
-        message: err.message,
+  /** @type import('express').Request */ req,
+  /** @type import('express').Response */ res
+) => {
+  const query = req.query.new;
+  try {
+    const image = query
+      ? await Image.find().sort({ _id: -1 })
+      : await Image.find();
+    if (!image) {
+      const response = res.status(403).json({
+        status: "fail",
+        message: "Image not found",
       });
       return response;
     }
-  };
+    const response = res.status(200).json({
+      status: "success",
+      data: image,
+    });
+    return response;
+  } catch (err) {
+    const response = res.status(500).json({
+      status: "fail",
+      message: err.message,
+    });
+    return response;
+  }
+};
 
-  
 export const deleteImage = async (
-    /** @type import('express').Request */ req,
-    /** @type import('express').Response */ res,
-  ) => {
-    const { id } = req.params;
-    try {
-      const image = await Image.findByIdAndDelete(id.replace(':', ''));
-      if (!image) {
-        const response = res.status(403).json({
-          status: 'fail',
-          message: 'image not found',
-        });
-        return response;
-      }
-      const response = res.status(200).json({
-        status: 'success',
-        message: 'image has been deleted',
-      });
-      return response;
-    } catch (err) {
-      const response = res.status(500).json({
-        status: 'fail',
-        message: err.message,
+  /** @type import('express').Request */ req,
+  /** @type import('express').Response */ res
+) => {
+  const { id } = req.params;
+  try {
+    const image = await Image.findByIdAndDelete(id.replace(":", ""));
+    if (!image) {
+      const response = res.status(403).json({
+        status: "fail",
+        message: "image not found",
       });
       return response;
     }
-  };
-  
-  export const getImageById = async (
-    /** @type import('express').Request */ req,
-    /** @type import('express').Response */ res,
-  ) => {
-    console.log("OK HERE")
-    const { id } = req.params;
-    try {
-      const image = await Image.findById(id.replace(':', ''));
-      if (!image) {
-        const response = res.status(403).json({
-          status: 'fail',
-          message: 'image not found',
-        });
-        return response;
-      }
-      const { password, ...others } = image._doc;
-      const response = res.status(200).json({
-        status: 'success',
-        data: others,
-      });
-      return response;
-    } catch (err) {
-      const response = res.status(500).json({
-        status: 'fail',
-        message: err.message,
+    const response = res.status(200).json({
+      status: "success",
+      message: "image has been deleted",
+    });
+    return response;
+  } catch (err) {
+    const response = res.status(500).json({
+      status: "fail",
+      message: err.message,
+    });
+    return response;
+  }
+};
+
+export const getImageById = async (
+  /** @type import('express').Request */ req,
+  /** @type import('express').Response */ res
+) => {
+  console.log("OK HERE");
+  const { id } = req.params;
+  try {
+    const image = await Image.findById(id.replace(":", ""));
+    if (!image) {
+      const response = res.status(403).json({
+        status: "fail",
+        message: "image not found",
       });
       return response;
     }
-  };
+    const { password, ...others } = image._doc;
+    const response = res.status(200).json({
+      status: "success",
+      data: others,
+    });
+    return response;
+  } catch (err) {
+    const response = res.status(500).json({
+      status: "fail",
+      message: err.message,
+    });
+    return response;
+  }
+};
+
+export const insertImage = async (
+  /** @type import('express').Request */ req,
+  /** @type import('express').Response */ res
+) => {
+    const title = req.body["title"];
+    const desc = req.body["desc"];
+    const imgSrc = req.body["imgSrc"];
+    try {
+    const image = new Image({
+      title,
+      desc,
+      imgSrc,
+    });
+    const result = await image.save();
+    const response = res.status(200).json({
+      status: "success",
+      data: result,
+    });
+    return response;
+  } catch (err) {
+    const response = res.status(500).json({
+      status: "failed",
+      message: "failed to insert image" + err.message,
+    });
+    return response;
+  }
+};
--- src/routes/image.routes.js
+++ src/routes/image.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import {
-    getImages, deleteImage, getImageById,
+    getImages, deleteImage, getImageById, insertImage,
 } from '../controllers/image.controllers.js';
 
 const router = express.Router();
 router.get('/', getImages);
 router.delete('/:id', deleteImage);
 router.get('/:id', getImageById);
+router.post('/', insertImage);
 
 export default router;
