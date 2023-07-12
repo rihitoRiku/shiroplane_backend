edc16d0ad10d5d5ebfba8d36c3ce31a856e38467
slvally <fieshidiq@gmail.com>
2023-07-12 12:34:54 +0700

    checkpoint

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import express from "express";
+import * as dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
-import authRouter from './src/routes/auth.routes.js';
-import imageRouter from './src/routes/image.routes.js';
+import authRouter from "./src/routes/auth.routes.js";
+import imageRouter from "./src/routes/image.routes.js";
 
 const init = () => {
   // setting up the server
   const server = express();
-  server.use(bodyParser.json({ limit: '10mb', extended: true }));
-  server.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+  server.use(bodyParser.json({ limit: "10mb", extended: true }));
+  server.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
   server.use(cors());
   server.use(helmet());
-  server.use(morgan('common'));
+  server.use(morgan("common"));
 
-  // register the routes
-//   server.use('/auth', authRouter);
-  server.use('/images', imageRouter);
-  server.get('/', (req, res) => {
+  //   server.use('/auth', authRouter);
+  server.use("/images", imageRouter);
+  server.get("/", (req, res) => {
     res.json({
-      status: 'success',
-      message: 'welcome to our API service'
+      status: "success",
+      message: "welcome to our API service",
     });
   });
 
-
-  server.use('/', authRouter);
-
+  server.use("/auth", authRouter);
 
   // get env from .env file
   dotenv.config();
   // get port from .env
   const PORT = process.env.PORT || 5000;
   // start the server
-  mongoose.connect(process.env.MONGOOSE_CONNECT_URL)
-    .then(() => server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)))
+  mongoose
+    .connect(process.env.MONGOOSE_CONNECT_URL)
+    .then(() =>
+      server.listen(PORT, () =>
+        console.log(`Server running on http://localhost:${PORT}`)
+      )
+    )
     .catch((error) => console.log(error.message));
 };
 
