979aea7be60c73d7879da0b2e7b0d21be34c0590
slvally <fieshidiq@gmail.com>
2023-08-06 14:32:05 +0700

    autha

--- src/routes/image.routes.js
+++ src/routes/image.routes.js
@@ -2,11 +2,12 @@ import express from 'express';
 import {
     getImages, deleteImage, getImageById, insertImage,
 } from '../controllers/image.controllers.js';
+import { verifyTokenAdmin, verifyTokenAndAuthorization } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 router.get('/', getImages);
-router.delete('/:id', deleteImage);
+router.delete('/:id',verifyTokenAndAuthorization, deleteImage);
 router.get('/:id', getImageById);
-router.post('/', insertImage);
+router.post('/',verifyTokenAndAuthorization, insertImage);
 
 export default router;
